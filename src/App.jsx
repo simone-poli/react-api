efd71678f409788239579eaf25a9be6757982936
simone-poli <polismn97@gmail.com>
2025-07-22 15:11:25 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+
+
+function App() {
+  
+  const url = "https://lanciweb.github.io/demo/api/actresses/" 
+  const [urlApi, setUrlApi] = useState(url)
+
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
